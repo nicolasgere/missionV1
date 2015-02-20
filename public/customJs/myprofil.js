4b093ba4c062baaa3e29915da6a41c4fe3adf59a
nicolasgere <nicolas.gere@hotmail.fr>
2015-02-19 23:01:55 -0500

    test

---
+++ public/customJs/myprofil.js
@@ -0,0 +1,54 @@
+
+
+var ViewModel = function() {
+	var self = this;
+
+	self.descIsModif = ko.observable(false);
+	self.descModif = function (){
+		self.descIsModif(true);	
+	}
+	self.desc = ko.observable(desc1);
+	self.descTemp = ko.observable(desc1);
+
+	self.descEnreg = function(){
+		$.ajax( {
+			url: 'updateDesc',
+			type: 'POST',
+			data: {desc:self.descTemp()},
+			success: function(){
+				self.desc(self.descTemp());
+				self.descIsModif(false);
+			}
+		} );
+		
+	}
+	self.newName = ko.observable();
+	self.newPrice = ko.observable();
+
+	self.imageSrc = ko.observable();
+	self.imageSrc.subscribe(function(){
+		$("#imgForm").submit();
+	});
+
+}
+
+
+
+window.onload = function(){
+	ko.applyBindings(new ViewModel());
+
+	$( '#imgForm' )
+	.submit( function( e ) {
+		$.ajax( {
+			url: 'updateImage',
+			type: 'POST',
+			data: new FormData( this ),
+			processData: false,
+			contentType: false,
+			success: function(){
+				location.reload()
+			}
+		} );
+		e.preventDefault();
+	} );
+};
