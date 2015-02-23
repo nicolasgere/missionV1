17df8c3830e52d614dea12c560672bfa1e38ab04
nicolasgere <nicolas.gere@hotmail.fr>
2015-02-22 21:25:39 -0500

    ok on delete

--- public/customJs/myprofil.js
+++ public/customJs/myprofil.js
@@ -1,13 +1,14 @@
-function meal(data){
-var self = this;
-self.name = ko.observable(data.name);
-self.price = ko.observable(data.price);
-self.category = ko.observable(data.cat);
-self.description = ko.observable(data.desc);
-self.realImage = ko.observable(data.img);
-self.mealId = ko.observable(data.MealId);
-self.supprimer = function(){
-	$.ajax( {
+function meal(data,parent){
+	var self = this;
+	self.parent = parent;
+	self.name = ko.observable(data.name);
+	self.price = ko.observable(data.price);
+	self.category = ko.observable(data.cat);
+	self.description = ko.observable(data.desc);
+	self.realImage = ko.observable(data.img);
+	self.mealId = ko.observable(data.MealId);
+	self.supprimer = function(){
+		$.ajax( {
 			url: 'deleteMeal',
 			type: 'POST',
 			data: {id:self.mealId()},
@@ -15,135 +16,150 @@ self.supprimer = function(){
 				location.reload()
 			}
 		} );
-};
+	};
+	self.modifyMeal = function(){
+		self.parent.newName(self.name());
+		self.parent.newPrice(self.price());
+		self.parent.newCategory(self.category());
+		self.parent.newDescription(self.description());
+		self.parent.selectMeal(self);
+		self.parent.newRealImage(self.realImage());
+	}
+
 };
 
 var ViewModel = function() {
 	var self = this;
 	self.meal1=ko.observable();
-    self.meal2=ko.observable();
-    self.meal3=ko.observable();
-    self.meal4=ko.observable();
+	self.meal2=ko.observable();
+	self.meal3=ko.observable();
+	self.meal4=ko.observable();
+	self.newName = ko.observable();
+	self.nomImage = ko.observable();
+	self.selectMeal = ko.observable();
+	self.newPrice = ko.observable();
+	self.newCategory = ko.observable();
+	self.newDescription = ko.observable();
+	self.newImage = ko.observable();
+	self.cible = ko.observable();
+	self.fichier = ko.observable();
+	self.newRealImage = ko.observable();
 
 	self.descIsModif = ko.observable(false);
 	self.descModif = function (){
 		self.descIsModif(true);	
 	}
+	self.nomImage.subscribe(function(){
+		self.fichier(self.nomImage().split(".")[1]);
+		if(self.fichier()!="jpg" && self.fichier()!="jpeg" && self.fichier()!="png"){
+			alert("Pas bon fichier");
+			self.nomImage();
+		}
+
+	});
 	self.desc = ko.observable(desc1);
 	self.descTemp = ko.observable(desc1);
 
-	self.descEnreg = function(){
-		$.ajax( {
-			url: 'updateDesc',
-			type: 'POST',
-			data: {desc:self.descTemp()},
-			success: function(){
-				self.desc(self.descTemp());
-				self.descIsModif(false);
-			}
-		} );
-		
-	}
+
 
 	self.availableCat = ko.observableArray(["Entre","Plat","Dessert"]);
-	self.newName = ko.observable();
-	self.newPrice = ko.observable();
-	self.newCategory = ko.observable();
-    self.newDescription = ko.observable();
-	self.newImage = ko.observable();
-	self.cible = ko.observable();
-	self.newRealImage = ko.observable();
-	self.changeCible = function(cible){
-    if(cible==1){
-    	self.cible = self.meal1;
-    }else if(cible==2){
-    	self.cible = self.meal2;
-    }else if(cible==3){
-    	self.cible = self.meal3;
-    }else if(cible==4){
-    	self.cible = self.meal4;
-    }
+	
+	self.changeCible = function(){
+		if(!self.meal1()){
+			self.cible = self.meal1;
+		}else if(!self.meal2()){
+			self.cible = self.meal2;
+		}else if(!self.meal3()){
+			self.cible = self.meal3;
+		}else if(!self.meal4()){
+			self.cible = self.meal4;
+		}
+		self.newName("");
+		self.newPrice("");
+		self.newCategory("");
+		self.newDescription("");
+		self.newImage("");
+		self.newRealImage("");
 	};
-
-    self.createMeal = function(){
-    	var dataM = {"name":self.newName(),
-			"price":self.newPrice(),
-			"cat":self.newCategory(),
-			"desc":self.newDescription(),
-			"img":self.newRealImage()
-		          }
-    		$.ajax( {
-			url: 'createMeal',
-			type: 'POST',
-			data:dataM,
-			success: function(data){
-				self.cible(new meal(dataM));
-				self.newPrice("");
-				self.newCategory(null);
-				self.newDescription("");
-				self.newImage("");
-				self.newName("");
-				 $('#myModal').modal('toggle');
-			}
-		} );
-    };
-    $.ajax({
-			url: 'getMeal',
-			type: 'GET',
-			success: function(data){
-				if(data[0]){
-					self.meal1(new meal(data[0]));
-				}
-				if(data[1]){
-					self.meal2(new meal(data[1]));
-				}
-				if(data[2]){
-					self.meal3(new meal(data[2]));
-				}
-				if(data[3]){
-					self.meal4(new meal(data[3]));
-				}
-			}
-	});
-
-	self.imageSrc = ko.observable();
-	self.imageSrc.subscribe(function(){
-		$("#imgForm").submit();
-	});
-	self.newImage.subscribe(function(){
-	$("#imgForm2").submit();
-	});
-
-	$( '#imgForm' )
-	.submit( function( e ) {
-		$.ajax( {
-			url: 'updateImage',
-			type: 'POST',
-			data: new FormData( this ),
-			processData: false,
-			contentType: false,
-			success: function(){
-				location.reload()
-			}
-		} );
-		e.preventDefault();
+	self.updateMeal = function(){
+		var dataM = {"name":self.newName(),
+		"price":self.newPrice(),
+		"cat":self.newCategory(),
+		"desc":self.newDescription(),
+		"img":self.newRealImage(),
+		"id": self.selectMeal().mealId()
+	}
+	$.ajax( {
+		url: 'updateMeal',
+		type: 'POST',
+		data:dataM,
+		success: function(data){
+			self.selectMeal().name(self.newName());
+			self.selectMeal().price(self.newPrice());
+			self.selectMeal().category(self.newCategory());
+			self.selectMeal().description(self.newDescription());
+			self.selectMeal().realImage(self.newRealImage());
+			$('#updateModal').modal('toggle');
+		}
 	} );
-	$( '#imgForm2' )
-	.submit( function( e ) {
-		$.ajax( {
-			url: 'loadImage',
-			type: 'POST',
-			data: new FormData( this ),
-			processData: false,
-			contentType: false,
-			success: function(data){
-				self.newRealImage(data);
-			}
-		} );
-		e.preventDefault();
-	} );
+
 }
+self.createMeal = function(){
+	var dataM = {"name":self.newName(),
+	"price":self.newPrice(),
+	"cat":self.newCategory(),
+	"desc":self.newDescription(),
+	"img":cropper.getAvatar(),
+	"ext":self.fichier()
 
+}
+$.ajax( {
+	url: 'createMeal',
+	type: 'POST',
+	data:dataM,
+	success: function(data){
+		dataM.img=data;
+		self.cible(new meal(dataM));
+		self.newPrice("");
+		self.newCategory(null);
+		self.newDescription("");
+		self.newImage("");
+		self.newName("");
+		$('#myModal').modal('toggle');
+	}
+} );
+};
+self.descEnreg = function(){
+	$.ajax( {
+		url: 'updateDesc',
+		type: 'POST',
+		data: {desc:self.descTemp()},
+		success: function(){
+			self.desc(self.descTemp());
+			self.descIsModif(false);
+		}
+	} );
+
+}
+$.ajax({
+	url: 'getMeal',
+	type: 'GET',
+	success: function(data){
+		if(data[0]){
+			self.meal1(new meal(data[0],self));
+		}
+		if(data[1]){
+			self.meal2(new meal(data[1],self));	
+		}
+		if(data[2]){
+			self.meal3(new meal(data[2],self));
+		}
+		if(data[3]){
+			self.meal4(new meal(data[3],self));
+		}
+	}
+});
+}
 
 
 window.onload = function(){
---
+++ public/customJs/mysettings.js
@@ -0,0 +1,91 @@
+
+var ViewModel = function() {
+	var self = this;
+	self.nom=ko.observable(nom);
+	self.prenom=ko.observable(prenom);
+	self.email=ko.observable(email);
+	self.ville=ko.observable(ville);
+	self.arron=ko.observable(arron);
+	self.desc=ko.observable(desc);
+	self.newNom=ko.observable(nom);
+	self.newPrenom=ko.observable(prenom);
+	self.newEmail=ko.observable(email);
+	self.newEVlle=ko.observable(ville);
+	self.newArron=ko.observable(arron);
+	self.newDesc=ko.observable(desc);
+	self.editNom = ko.observable(true);
+	self.editPrenom = ko.observable(true);
+	self.editVille = ko.observable(true);
+	self.editDesc = ko.observable(true);
+	self.editEmail = ko.observable(true);
+
+self.save = function(){
+var dataM = {"nom":self.nom(),
+		"prenom":self.prenom(),
+		"email":self.email(),
+		"ville":self.ville(),
+		"desc":self.desc(),
+		"arron":self.arron(),
+		"desc": self.desc()
+	}
+	$.ajax( {
+		url: 'updateSettings',
+		type: 'POST',
+		data:dataM,
+		success: function(data){
+			self.editNom(true);
+			self.editPrenom(true);
+			self.editVille(true);
+			self.editDesc(true);
+			self.editEmail(true);
+			self.editArron(true);
+
+		}
+	} );
+}
+
+self.editArron = ko.observable(true);
+self.changeNom = function(){
+	self.editNom(false)
+}
+self.test= function(){
+            var img = cropper.getAvatar()
+	var data = img;
+	$.ajax( {
+		url: 'test',
+		type: 'POST',
+		data:{blob:data},
+		success: function(data){
+
+       $("#imgProfil").attr('src',"imgProfil/"+data);
+		$('#updateModal').modal('toggle');
+
+		}
+	} );
+
+}
+self.changePrenom = function(){
+	self.editPrenom(false)
+}
+self.changeDesc = function(){
+	self.editDesc(false)
+}
+self.changeVille = function(){
+	self.editVille(false)
+}
+self.changeArron = function(){
+	self.editArron(false)
+}
+self.changeEmail = function(){
+	self.editEmail(false)
+}
+
+}
+
+
+
+window.onload = function(){
+	ko.applyBindings(new ViewModel());
+
+
+};
